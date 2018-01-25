32372138f8d0235be2f9d2d6ea8a3f36df8de9e1
Ludaris <Luda@github.com>
2018-01-25 14:35:40 +0200

    SpellMovement_Bullet (Test)

---
+++ Assets/Scripts/Spells/SpellMovement_Bullet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellMovement_Bullet : MonoBehaviour {
+
+    private Vector3 _spellPosition;
+    private Vector3 _target;
+    private float _startTime;
+    private float path;
+
+    public float distance = 2f;
+    public float speed = 50f;
+
+	void Start () {
+        _startTime = Time.time;
+        _spellPosition = GetComponent<Transform>().position;
+        _target = GameObject.Find("MouseController").GetComponent<MouseController>().mousePosition * distance; 
+	}
+
+    private void FixedUpdate()
+    {
+        path = speed * (Time.time - _startTime); 
+        transform.position = Vector3.Lerp(_spellPosition, _target, path*Time.deltaTime);
+        if (transform.position == _target)
+            Destroy(gameObject, 1);
+    }
+
+}
