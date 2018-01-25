f44443002b417fa3036f5670aec72465798217b7
Ludaris <Luda@github.com>
2018-01-25 19:44:54 +0200

    SpellMovement_Bullet Upgrade

--- Assets/Scripts/Spells/Movements/SpellMovement_Bullet.cs
+++ Assets/Scripts/Spells/Movements/SpellMovement_Bullet.cs
@@ -5,25 +5,32 @@ using UnityEngine;
 public class SpellMovement_Bullet : MonoBehaviour {
 
     private Vector3 _spellPosition;
+    private Vector3 _mousePosition;
     private Vector3 _target;
+    private Vector3 _direction;
     private float _startTime;
     private float path;
 
-    public float distance = 2f;
-    public float speed = 50f;
+    public Spell spell;
 
 	void Start () {
+        _mousePosition = GameObject.Find("MouseController").GetComponent<MouseController>().mousePosition;
         _startTime = Time.time;
         _spellPosition = GetComponent<Transform>().position;
-        _target = GameObject.Find("MouseController").GetComponent<MouseController>().mousePosition * distance; 
 	}
 
     private void FixedUpdate()
     {
-        path = speed * (Time.time - _startTime); 
+        _direction = ((_mousePosition - _spellPosition) / (_mousePosition - _spellPosition).magnitude);
+        _target = _direction * spell.distance + _spellPosition;
+        path = spell.speed * (Time.time - _startTime); 
+
         transform.position = Vector3.Lerp(_spellPosition, _target, path*Time.deltaTime);
+
         if (transform.position == _target)
             Destroy(gameObject, 1);
+
     }
+
 
 }
