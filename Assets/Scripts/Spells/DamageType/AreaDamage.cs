b904f83137a0bd3476e24673dcb28c8f4e546211
Ludaris <Luda@github.com>
2018-01-29 16:09:02 +0200

    AreaDamage cs Added

---
+++ Assets/Scripts/Spells/DamageType/AreaDamage.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AreaDamage : MonoBehaviour
+{
+    SpellContainer _spell;
+
+    void Start()
+    {
+        _spell = gameObject.GetComponent<SpellContainer>();
+        _spell.SpellConteinerLoad();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Death();
+    }
+
+    public void Death()
+    {
+        RaycastHit[] elements = Physics.SphereCastAll(gameObject.transform.position, _spell.castArea, transform.forward, 0f);
+        foreach (RaycastHit hit in elements)
+        {
+            if (hit.transform.gameObject != gameObject)
+            {
+                ElementHit(hit.collider);
+            }
+        }
+        Destroy(gameObject);
+
+    }
+
+    void ElementHit(Collider other)
+    {
+        if (other.tag == "background_element")
+        {
+            other.gameObject.GetComponent<Background_Stats>().currentHP -= _spell.damage;
+        }
+
+        if (other.tag == "Player")
+        {
+            other.gameObject.GetComponent<PlayerStats>().PlayerHealthPointUpdate(-(_spell.damage));
+        }
+
+        if (other.tag == "spell") { 
+            if (other.gameObject.GetComponent<SpellContainer>().spellHP > _spell.spellHP)
+            { 
+                other.gameObject.GetComponent<SpellContainer>().spellHP -= _spell.spellHP;
+            }
+            else if (other.gameObject.GetComponent<SpellContainer>().spellHP < _spell.spellHP)
+            {
+                _spell.spellHP -= other.gameObject.GetComponent<SpellContainer>().spellHP;
+            }
+            else if (other.gameObject.GetComponent<SpellContainer>().spellHP == _spell.spellHP)
+            {
+                Destroy(other.gameObject);
+            }
+        }
+    }
+}
