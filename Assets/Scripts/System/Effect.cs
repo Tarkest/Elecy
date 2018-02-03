f85aa530ecd6b0d029a860ad3d112855477b3f35
Tarkest <33873988+Tarkest@users.noreply.github.com>
2018-02-03 20:08:02 +0200

    Buff basic added

---
+++ Assets/Scripts/System/Effect.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Buff/Debuff", menuName = "Effect")]
+public class Effect : ScriptableObject {
+
+    public string effectName;
+    public string effectDescription;
+    public string stateType;
+    public int healthMod;
+    public int synergyMod;
+    public int damageMod;
+    public float movespeedMod;
+    public float duration;
+    public float modFrequency;
+    public Image buffIcon;
+	
+}
---
+++ Assets/Scripts/System/EffectController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class EffectController : MonoBehaviour {
+
+    public Effect effect;
+
+    private string stateType;
+    private int healthMod;
+    private int synergyMod;
+    private int damageMod;
+    private float movespeedMod;
+    private float duration;
+    private float modFrequency;
+    private Image buffIcon;
+
+    private float _durationCounter = 0f;
+
+	void Awake () {
+        stateType = effect.stateType;
+        healthMod = effect.healthMod;
+        synergyMod = effect.synergyMod;
+        damageMod = effect.damageMod;
+        movespeedMod = effect.movespeedMod;
+        duration = effect.duration;
+        buffIcon = effect.buffIcon;
+        modFrequency = effect.modFrequency;
+	    StatsModify();
+    }
+	
+
+	void Update () {
+        _durationCounter += Time.deltaTime;
+
+        if (_durationCounter >= duration)
+            RemoveEffect();
+	}
+
+    void StatsModify()
+    {
+        GetComponent<PlayerMovement>().speed += movespeedMod;
+    }
+
+    void FrequentChange()
+    {
+
+    }
+
+    void RemoveEffect()
+    {
+        GetComponent<PlayerMovement>().speed -= movespeedMod;
+    }
+}
