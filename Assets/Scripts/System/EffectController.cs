cb966835fa1f262e57e576493b0ba0691051e7f2
Tarkest <33873988+Tarkest@users.noreply.github.com>
2018-02-06 19:52:24 +0200

    Effect controller update

--- Assets/Scripts/System/EffectController.cs
+++ Assets/Scripts/System/EffectController.cs
@@ -5,53 +5,66 @@ public class EffectController : MonoBehaviour {
 
     public Effect effect;
 
-    private bool isStunned;
-    private bool isCast;
-    private bool isStucked;
-    private int healthMod;
-    private int synergyMod;
-    private int damageMod;
-    private float movespeedMod;
-    private float duration;
-    private float modFrequency;
-    private Image buffIcon;
+    private bool _isStunned;
+    private bool _isCast;
+    private bool _isStucked;
+    private bool _isStunStackable;
+    private bool _isHpModStackable;
+    private bool _isMsModStackable;
 
-    private float _durationCounter = 0f;
+    private int _healthMod;
+    private int _synergyMod;
+    private int _damageMod;
+
+    private float _movespeedMod;
+    private float _stunduration;
+    private float _currentstunduration;
+    private float _stunDurationModifyer;
+    private float _castduration;
+    private float _stuckduration;
+    private float _modFrequency;
+
+    [System.NonSerialized]
+    public Image buffIcon;
+
+    private float _stunDurationCounter = 0f;
+    private float _castDurationCounter = 0f;
+    private float _stuckdurationcounter = 0f;
 
 	public void EffectLoad () {
-        isStunned = effect.isStunning;
-        isCast = effect.isCasting;
-        isStucked = effect.isStucking;
-        healthMod = effect.healthMod;
-        synergyMod = effect.synergyMod;
-        damageMod = effect.damageMod;
-        movespeedMod = effect.movespeedMod;
-        duration = effect.duration;
+        _isStunned = effect.isStunning;
+        _isCast = effect.isCasting;
+        _isStucked = effect.isStucking;
+        _isStunStackable = effect.isStunStackable;
+        _isHpModStackable = effect.isHpModStackable;
+        _isMsModStackable = effect.isMsModStackable;
+        _healthMod = effect.healthMod;
+        _synergyMod = effect.synergyMod;
+        _damageMod = effect.damageMod;
+        _movespeedMod = effect.movespeedMod;
+        _stunduration = effect.stunduration;
+        _castduration = effect.castduration;
+        _stuckduration = effect.stuckduration;
         buffIcon = effect.buffIcon;
-        modFrequency = effect.modFrequency;
-    }
-	
-
-	void Update () {
-        _durationCounter += Time.deltaTime;
-
-        if (_durationCounter >= duration)
-            RemoveEffect();
-	}
-
-    void StatsModify()
-    {
-        GetComponent<PlayerMovement>().speed += movespeedMod;
+        _modFrequency = effect.modFrequency;
     }
 
-    void FrequentChange()
-    {
 
-    }
-
-    void RemoveEffect()
+    void Update()
     {
-        GetComponent<PlayerMovement>().speed -= movespeedMod;
-        Destroy(this);
+        if(_isStunned)
+        {
+            _stunDurationCounter += Time.deltaTime;
+            if (!GetComponent<PlayerStats>().isStunned)
+            {
+                _currentstunduration = _stunduration;
+                GetComponent<PlayerStats>().isStunned = true;
+            }
+            else if(GetComponent<PlayerStats>().isStunned && _isStunStackable)
+            {
+                _currentstunduration += _stunduration;
+            }
+
+        }
     }
 }
