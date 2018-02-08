feda224df6d5e8a42e996a2b438d2a32930c68ec
Tarkest <33873988+Tarkest@users.noreply.github.com>
2018-02-08 17:35:30 +0200

    Revert "Revert "Effect controller update""
    This reverts commit 851591540dee1f046a625a57dc83f0f07ba7954a.

--- Assets/Scripts/System/Effect.cs
+++ Assets/Scripts/System/Effect.cs
@@ -6,15 +6,25 @@ public class Effect : ScriptableObject {
 
     public string effectName;
     public string effectDescription;
+
     public bool isStunning;
     public bool isCasting;
     public bool isStucking;
+    public bool isStunStackable;
+    public bool isHpModStackable;
+    public bool isMsModStackable;
+
     public int healthMod;
     public int synergyMod;
     public int damageMod;
+
     public float movespeedMod;
-    public float duration;
+    public float stunduration;
+    public float castduration;
+    public float stuckduration;
+    public float effectduration;
     public float modFrequency;
+
     public Image buffIcon;
 	
 }
