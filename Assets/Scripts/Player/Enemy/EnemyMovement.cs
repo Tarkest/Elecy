b389c14fcbe0db906832b56aae03fe820ae7aeb7
Tarkest <33873988+Tarkest@users.noreply.github.com>
2018-03-10 19:06:28 +0200

    Enemy Movement update

--- Assets/Scripts/Player/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Player/Enemy/EnemyMovement.cs
@@ -7,47 +7,60 @@ public class EnemyMovement : MonoBehaviour {
 
     bool change = false;
     static bool start = false;
-    Vector3 pos;
+    Vector3 servPos;
     Quaternion rot;
-    static Vector3 startpos = new Vector3();
+    static Vector3 transPos = new Vector3();
     static Quaternion startrot = new Quaternion();
-    float startTime;
-    float distance = 1f;
-    float angle = 1f;
-    float fracJourney;
-    float speed = 20f;
-    float rotSpeed = 0.2f;
-    Timer timer;
+    Vector3 prevPos;
+    float currentDistance;
+    float realDistance;
+    float progress;
+
+    void Update()
+    {
+        if(prevPos == Vector3.zero)
+        {
+            prevPos = servPos;
+        }
+        realDistance = Vector3.Distance(prevPos, servPos);
+        currentDistance = Vector3.Distance(transPos, servPos);
+        if(realDistance != 0)
+        {
+            progress = currentDistance / realDistance;
+        }
+        prevPos = servPos;
+
+        transPos = Vector3.Lerp(transPos, servPos, progress);
+    }
 
     void FixedUpdate()
     {
         if (change)
         {
             change = false;
-            startTime = Time.time;
         }
 
         if (start)
         {
-            fracJourney = (Time.time - startTime) * speed;
-            gameObject.transform.position = pos;
+
+            gameObject.transform.position = transPos;
             gameObject.transform.rotation = rot;
         }
     }
 
     public static void SetStartPos(Vector3 position, Quaternion rotation)
     {
-        startpos = position;
+        transPos = position;
         startrot = rotation;
     }
 
     public void SetTransform(Vector3 position, Quaternion rotation)
     {
-        startpos = GlobalObjects.enemyPos;
+        transPos = GlobalObjects.enemyPos;
         startrot = GlobalObjects.enemyRot;
-        pos = position;
+        servPos = position;
         rot = rotation;
-        distance = Vector3.Distance(startpos, pos);
+
         //angle = Quaternion.Angle(startrot, rot);
         change = true;
         start = true;
